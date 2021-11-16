c14f219381f340530e76a4abca0764932b47c757
donielacosta1995 <donielacosta1995@gmailcom>
2021-11-16 11:38:22 -0400

    En props definimos las propiedades que el componente padre le va a enviar al hijo. Los props se utilizan igual que las propiedades de data, se utilizan a través de this. El componente hijo no puede escribir o modificar los props que le mande el padre. . El nombre del componente se escribe en Pascal Case, donde cada palabra inicia con la primera letra en mayúscula. Al momento de llamar al componente en el HTML, Vue parsea el nombre a Kebab Case, una nomenclatura con guiones lo cual permite mantener una consistencia con los tags HTML. . Se recomienda mantener la consistencia entre los lenguajes, utilizar Pascal Case dentro de JavaScript: CoinDetail ; y Kebab Case dentro de HTML: coin-detail. . Para pasarle las propiedades al componente se hace desde el HTML usando la directiva v-bind: v-bind:change-percent = “changePercent”. Aquí también aplica lo del Kebab Case. . Los componentes de Vue necesitan tener un componente padre único que encierre a todos los demás. . Cuando las propiedades tienen un elemento central común es más cómodo trabajarlas mediantes un objeto, así evitamos tener que estar pasando tantos props.

--- app.js
+++ app.js
@@ -1,45 +1,102 @@
-new Vue({
-  el:'#app',
+Vue.component('CoinDetail',{
 
-  data() { 
-    return {
-      name: 'Bitcoin',
-      Symbol:'BTC',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent:10,
-      color:'f4f4f4',
-      price:9000,
-      pricesWithDays: [
-                  { day: 'Lunes', value: 8400 },
-                  { day: 'Martes', value: 7900 },
-                  { day: 'Miercoles', value: 8200 },
-                  { day: 'Jueves', value: 9000 },
-                  { day: 'Viernes', value: 9400 },
-                  { day: 'Sabado', value: 10000 },
-                  { day: 'Domingo', value: 10200 },
-                ],
-      showPrices:false
+  props:['coin'],
+
+  data(){
+    return{
+      showPrices:false,
+      value:0,
+    }
+  },
+
+  methods:{
+    toggleShowPrices(){
+      this.showPrices =!this.showPrices
     }
   },
 
   computed:{
     title(){
-      return `${this.name} - ${this.Symbol}`
+      return `${this.coin.name} - ${this.coin.Symbol}`
+    },
+    converteValue(){
+      if(!this.value){
+        return 0
+      }
+      return this.value/this.coin.price
     }
   },
 
-  watch:{
-    showPrices(newVal,oldVal){
-      console.log(newVal,oldVal)
-    }
-  },
-  methods: {
-    toggleShowPrices(){
-      this.showPrices = !this.showPrices
+  template:`
 
-      this.color= this.color.split('').reverse().join('')
+    <div>
+        <img 
+        v-on:mouseover="toggleShowPrices" 
+        v-on:mouseout="toggleShowPrices" 
+        v-bind:src="coin.img" v-bind:alt="coin.name" 
+        >
+
+      <h1 v-bind:class="coin.changePercent >0 ? 'green' : 'red'">
+        {{title}}
+        <span v-if="coin.changePercent > 0">a</span>
+        <span v-else-if ="coin.changePercent < 0" > (y)</span>
+        <span v-else> si</span>
+
+        <span v-on:click="toggleShowPrices">{{showPrices ?'🙈':'🙊'}}</span>
+      </h1>
+
+      <input type="number" v-model="value">
+      <span>{{converteValue}}</span>
+
+      <ul v-show="showPrices">
+        <li 
+          class="uppercase"
+          v-bind:class="{ orange:p.value=== coin.price, red:p.value < coin.price, green:p.value > coin.price}"
+          v-for="(p,i) in coin.pricesWithDays"
+          v-bind:key="p.day">
+            {{i}}-{{p.day}}-{{p.value}} 
+        </li>
+     </ul>
+    </div>
+    
+    
+  `
+})
+
+new Vue({
+  el:'#app',
+
+  data() { 
+    return {
+      btc:{
+        name: 'Bitcoin',
+        Symbol:'BTC',
+        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+        changePercent:10,
+        price:9000,
+        pricesWithDays: [
+                    { day: 'Lunes', value: 8400 },
+                    { day: 'Martes', value: 7900 },
+                    { day: 'Miercoles', value: 8200 },
+                    { day: 'Jueves', value: 9000 },
+                    { day: 'Viernes', value: 9400 },
+                    { day: 'Sabado', value: 10000 },
+                    { day: 'Domingo', value: 10200 },
+                  ],
+      },
+     
+      color:'f4f4f4'
+ 
     }
   },
+
+  // methods: {
+  //   toggleShowPrices(){
+  //     this.showPrices = !this.showPrices
+
+  //     this.color= this.color.split('').reverse().join('')
+  //   }
+  // },
 })
 
 // Vue.component('CoinDetail', {
